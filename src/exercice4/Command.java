2f89f1e8d30bdeee9b71336fb581f632f755e011
Le Gall Elouan <elouanlegall1005@gmail.com>
2026-04-08 10:51:50 +0200

    Restructuration suppression des anciens fichiers déplacés

--- src/exercice4/Command.java
+++
@@ -1,7 +0,0 @@
-package exercice4;
-
-import stree.parser.SNode;
-
-public interface Command {
-	abstract public Reference run(Reference receiver, SNode method);
-}
--- src/exercice5/Command.java
+++
@@ -1,7 +0,0 @@
-package exercice5;
-
-import stree.parser.SNode;
-
-public interface Command {
-	abstract public Reference run(Reference receiver, SNode method);
-}
